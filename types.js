8f1aae91e376ecba258236c1f8fe69a466db715f
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2023-10-25 22:03:24 +0200

    solution

---
+++ types.js
@@ -0,0 +1,18 @@
+//  function data(){
+//     return [1,2,3]
+//  }
+//  var tmp = data()
+//  var first = tmp[0]
+//  var second = tmp[1]
+//  var third = tmp[2]
+
+// destructuring
+ 
+function data(){
+    return [1,2,3]
+}
+var [
+    first,
+    second,
+    third
+] = data()
